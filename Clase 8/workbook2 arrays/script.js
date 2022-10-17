41500024b76e648543bddc31fa521913df5c81c4
jlap23 <114718457+jlap23@users.noreply.github.com>
2022-10-17 16:37:14 -0500

    master

---
+++ Clase 8/workbook2 arrays/script.js
@@ -0,0 +1,50 @@
+class Producto {
+    constructor(nombre,precio,detalle,cantidad){
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        this.detalle = detalle;
+        this.cantidad = cantidad;
+        this.disponible = true; 
+    }
+
+    sumarIva(){
+        return this.precio * 1.21;
+    }
+
+    Vender(){
+     this.disponible= false;
+    }
+
+    precioSugerido(){
+    return this.precio * 1.21 * 1.25;
+    }
+
+}
+
+var arrayProductos = [];
+do{
+    var comprobacion = prompt("Ingresa nombre de producto o fin para finalizar");
+    if (comprobacion === "fin" || comprobacion === "FIN" || comprobacion === "Fin"){
+        break;
+    }else{
+        nombreP= comprobacion;
+        var precioP = prompt("Ingresa precio");
+        var detalleP = prompt("Ingresa detalle");
+        var cantidadP = prompt("Ingresa cantidad");
+        arrayProductos.push(new Producto(nombreP,precioP,detalleP,cantidadP));
+    }
+}
+    while (comprobacion != "fin" || comprobacion != "FIN" || comprobacion != "Fin")
+    console.log(arrayProductos);
+
+//DOCUMENT.WRITE("<h3>EL PRECIO DEL COSTO DEL PRODUCTO A CALCULAR ES: "+precioCostoM+ "</H3>")
+    document.write("<h3> El producto ingresado es: "+Producto.nombreP + "</h3>");
+    document.write("<h3> El detalle del producto ingresado es: "+Producto.detalleP + "</h3>");
+    document.write("<h3> La cantidad en stock del producto ingresado es: "+Producto.cantidad + "</h3>");
+    document.write("<h3> El precio del producto con IVA es: "+Producto.sumarIva() + "</h3>");
+
+    console.log(Producto.nombre);
+    console.log(Producto.detalle);
+    console.log(Producto.cantidad);
+    console.log(Producto.sumarIva());
+
